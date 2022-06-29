0cea6444647092eeff29a2a5b6863137204576bf
Kristiyan-Petkov <kristiyanp@gmail.com>
2022-06-29 14:31:38 +0200

    Add search

--- 04_Workshop ExpressJS and Templating/Cubicle-Workshop-Project/src/services/cubeService.js
+++ 04_Workshop ExpressJS and Templating/Cubicle-Workshop-Project/src/services/cubeService.js
@@ -2,6 +2,14 @@ const fs = require('fs/promises');
 const path = require('path');
 const cubes = require('../views/db.json');
 
+exports.getAll = (search = '', from = 0, to = 6) => {
+    let result = cubes
+        .filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
+        .filter(x => x.difficultyLevel >= from && x.difficultyLevel <= to);
+    
+    return result;
+};
+
 exports.getOne = (cubeId) => cubes[cubeId];
 
 exports.save = (cube) => {
