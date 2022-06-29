9890722f6fd3107e2832fa3f4d59f41db2583075
Kristiyan-Petkov <kristiyanp@gmail.com>
2022-06-29 15:00:30 +0200

    Memorize search fields

--- 04_Workshop ExpressJS and Templating/Cubicle-Workshop-Project/src/services/cubeService.js
+++ 04_Workshop ExpressJS and Templating/Cubicle-Workshop-Project/src/services/cubeService.js
@@ -2,8 +2,11 @@ const fs = require('fs/promises');
 const path = require('path');
 const cubes = require('../views/db.json');
 
-exports.getAll = (search = '', from = 0, to = 6) => {
-    let result = cubes
+exports.getAll = (search = '', fromInput, toInput) => {
+    const from = Number (fromInput) || 0;
+    const to = Number (toInput) || 6;
+
+    const result = cubes
         .filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
         .filter(x => x.difficultyLevel >= from && x.difficultyLevel <= to);
     
