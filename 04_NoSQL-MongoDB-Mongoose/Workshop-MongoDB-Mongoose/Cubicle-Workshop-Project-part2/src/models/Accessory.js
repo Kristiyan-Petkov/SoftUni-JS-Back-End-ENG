e8a80eb4dd65d8415690262f775ef66f608acd6f
Kristiyan-Petkov <kristiyanp@gmail.com>
2022-07-06 22:28:23 +0200

    Create accessory model

--- 04_NoSQL-MongoDB-Mongoose/Workshop-MongoDB-Mongoose/Cubicle-Workshop-Project-part2/src/models/Accessory.js
+++ 04_NoSQL-MongoDB-Mongoose/Workshop-MongoDB-Mongoose/Cubicle-Workshop-Project-part2/src/models/Accessory.js
@@ -1,5 +1,28 @@
 const mongoose = require('mongoose');
 
 const accessorySchema = new mongoose.Schema({
-    
+    name: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: {
+            validator: /^http/g,
+            message: 'Imgae URL should be a link'
+        }
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 120,
+    },
+    // cubes: {
+    //     // to do
+    // }
 })
+
+const Accessory = mongoose.model('Accessory', accessorySchema);
+
+module.exports = Accessory;
