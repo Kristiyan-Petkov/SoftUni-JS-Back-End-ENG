fd2bdb784b37a6c16b73ee52323d95af423bac4d
Kristiyan-Petkov <kristiyanp@gmail.com>
2022-07-07 12:49:24 +0200

    Activated the search bar (operation still on browser side, needs to be moved to server side)

--- 04_NoSQL-MongoDB-Mongoose/Workshop-MongoDB-Mongoose/Cubicle-Workshop-Project-part2/src/services/cubeService.js
+++ 04_NoSQL-MongoDB-Mongoose/Workshop-MongoDB-Mongoose/Cubicle-Workshop-Project-part2/src/services/cubeService.js
@@ -7,14 +7,14 @@ const Cube = require('../models/Cube');
 
 exports.getAll = async (search = '', fromInput, toInput) => {
     let cubes = await Cube.find().lean()
-    // const from = Number (fromInput) || 0;
-    // const to = Number (toInput) || 6;
+    const from = Number (fromInput) || 0;
+    const to = Number (toInput) || 6;
 
-    // const result = cubes
-    //     .filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
-    //     .filter(x => x.difficultyLevel >= from && x.difficultyLevel <= to);
+    const result = cubes
+        .filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
+        .filter(x => x.difficultyLevel >= from && x.difficultyLevel <= to);
     
-    return cubes;
+    return result;
 };
 
 exports.getOne = (cubeId) => Cube.findById(cubeId); 
