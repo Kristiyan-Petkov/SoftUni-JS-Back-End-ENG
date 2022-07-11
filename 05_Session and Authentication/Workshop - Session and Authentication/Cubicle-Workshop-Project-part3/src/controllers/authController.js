fe3b07726bb7f58b3ed0fe6825931adfef90a73f
Kristiyan-Petkov <kristiyanp@gmail.com>
2022-07-11 20:28:10 +0200

    added user login functionality

--- 05_Session and Authentication/Workshop - Session and Authentication/Cubicle-Workshop-Project-part3/src/controllers/authController.js
+++ 05_Session and Authentication/Workshop - Session and Authentication/Cubicle-Workshop-Project-part3/src/controllers/authController.js
@@ -19,8 +19,11 @@ router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
-router.post('/login', (req, res) => {
-    let userToLog = req.body;
+router.post('/login', async (req, res) => {
+    let result = await authService.login(req.body);
+    if (!result) {
+        return res.redirect('/404')
+    }
     res.redirect('/');
 
 })
--- 05_Session and Authentication/Workshop - Session and Authentication/Cubicle-Workshop-Project-part3/src/services/authService.js
+++ 05_Session and Authentication/Workshop - Session and Authentication/Cubicle-Workshop-Project-part3/src/services/authService.js
@@ -16,3 +16,20 @@ exports.register = async ({username, password, repeatPassword}) => {
 
     return createdUser;
 };
+
+exports.login = async ({username, password}) => {
+    let user = await User.findOne({username});
+
+    if (!user){
+        // TO DO add message
+        return false;
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (isValid){
+        return user;
+    } else {
+        return false;
+    }
+}
