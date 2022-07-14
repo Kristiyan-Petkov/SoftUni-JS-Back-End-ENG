f7dbe7c22c9d0ad87052d2aa84c3cb6492dfebb5
Kristiyan-Petkov <kristiyanp@gmail.com>
2022-07-14 11:44:11 +0200

    server-side error handling

--- 06_Validation and Error Handling/Workshop Validation and Error Handling/src/models/User.js
+++ 06_Validation and Error Handling/Workshop Validation and Error Handling/src/models/User.js
@@ -4,18 +4,22 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, 'Username must be your valid email'],
+        unique: [true, 'This username already exists'],
+        validate: /[a-zA-Z0-9]+/,
+        minLength: 5,
     },
     password: {
         type: String,
-        required: true,
+        required: [true, 'Password is required'],
+        minLength: [8, 'Your password should be at least 8 characters long'],
     }
 })
 
-userSchema.virtual('repeatPassword').set(function (value) {
-    if (this.password !== value) {
-        throw new Error('Repeat password must match password');
-    }
-})
+// userSchema.virtual('repeatPassword').set(function (value) {
+//     if (this.password !== value) {
+//         throw new Error('Repeat password must match password');
+//     }
+// })
 
 const User = mongoose.model('User', userSchema);
 
