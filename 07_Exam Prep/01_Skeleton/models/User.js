4eb93ad7f8b7366ed7720e01492a4709c5ee9af5
Kristiyan-Petkov <kristiyanp@gmail.com>
2022-07-19 16:55:42 +0200

    functionning username creation in db

--- 07_Exam Prep/01_Skeleton/models/User.js
+++ 07_Exam Prep/01_Skeleton/models/User.js
@@ -1,13 +1,26 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const { SALT_ROUNDS } = require('../config/env');
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-
+        //add uniqueness check and error message
     },
     password: {
         type: String,
+    },
+    address: {
+        type: String,
     }
+})
+
+userSchema.pre('save', function (next) {
+    bcrypt.hash(this.password, SALT_ROUNDS)
+        .then(hashedPassword => {
+            this.password = hashedPassword;
+            next();
+        })
 })
 
 const User = mongoose.model('User', userSchema);
