55f27b499cc5919eefb450d8736eb4dd9fd41b79
Kristiyan-Petkov <kristiyanp@gmail.com>
2022-08-04 23:19:11 +0200

    Primary setup - server, router, cors

---
+++ 07_Workshop REST API/server/index.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('./src/middlewares/cors');
+const furnitureController = require('./src/controllers/furniture');
+
+async function start() {
+    try {
+        const db = await mongoose.connect('mongodb://localhost:27017/furnitureREST');
+
+
+        console.log('DB Ready!');
+    } catch (err) {
+        console.log('Error connecting to database');
+        return process.exit(1);
+    }
+    const app = express();
+
+    app.use(express.json());
+    app.use(cors());
+    app.use('/data/catalog', furnitureController);
+
+    app.listen(3030, () => console.log('REST Service started on port 3030'));
+};
+
+start();
+
---
+++ 07_Workshop REST API/server/src/models/idem.js
@@ -0,0 +1,5 @@
+const { model, Schema } = require('mongoose');
+
+const itemSchema = new Schema {
+    
+}
